4a28b334921f454a040678dc2345ce03458775f9
Nikita <nikitak885@gmail.com>
2024-09-05 15:58:02 +0530

    main Model add

---
+++ app/model/user.py
@@ -0,0 +1,11 @@
+from .. import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
